9f198ad59518544ffee27e4e5bccba879edc8f5a
hendro <hendro.prabowo15@gmail.com>
2017-04-17 16:30:04 -0700

    add 2 tcp agent

--- sample3.tcl
+++ sample3.tcl
@@ -1,4 +1,4 @@
-set val(rp)     DSDV
+set val(rp)     DSR ;#routing protocol (DIFFUSION/RATE, DIFFUSION/PROB, DSDV, DSR, FLOODING, OMNIMCAST, AODV, TORA, M-DART, PUMA)
 set val(chan)   Channel/WirelessChannel
 set val(prop)   Propagation/TwoRayGround
 set val(netif)  Phy/WirelessPhy
@@ -6,12 +6,11 @@ set val(mac)    Mac/802_11
 set val(ifq)    Queue/DropTail/PriQueue
 set val(ll)     LL
 set val(ant)    Antenna/OmniAntenna
-set val(x)              670   ;# X dimension of the topography
-set val(y)              670   ;# Y dimension of the topography
+set val(x)              1000   ;# X dimension of the topography
+set val(y)              1000   ;# Y dimension of the topography
 set val(ifqlen)         50            ;# max packet in ifq
 set val(seed)           0.0
-set val(adhocRouting)   DSR
-set val(nn)             50             ;# how many nodes are simulated
+set val(nn)             20             ;# how many nodes are simulated
 set val(stop)           2000.0           ;# simulation time
 set val(speed)			25.0
 
@@ -43,19 +42,24 @@ $ns_ node-config -adhocRouting $val(rp) \
                          -movementTrace OFF
 
 for {set i 0} {$i < $val(nn) } {incr i} {
-                set node_($i) [$ns_ node ]
-                $node_($i) random-motion 1       ;# disable random motion
+                set node_($i) [$ns_ node]
+                # $node_($i) random-motion 0       ;# disable random motion
         } 
+
+# set color
+$node_(0) color "#FF0000"
 
 for {set i 0} {$i < $val(nn)} {incr i} {
     $node_($i) set X_ [expr rand()*500]
-    $node_($i) set Y_ [expr rand()*400]
+    $node_($i) set Y_ [expr rand()*500]
     $node_($i) set Z_ 0
 }
 
 # set label
-$ns_ at 0.0 "$node_(0) label Receiver"
-$ns_ at 0.0 "$node_(1) label Sender"
+$ns_ at 0.0 "$node_(0) label Sender"
+$ns_ at 0.0 "$node_(10) label Sender"
+$ns_ at 0.0 "$node_(1) label Receiver"
+$ns_ at 0.0 "$node_(11) label Receiver"
 
 # Generate movements and speed
 for {set i 0} {$i < $val(nn)} {incr i} {
@@ -64,16 +68,7 @@ for {set i 0} {$i < $val(nn)} {incr i} {
 	set rng_time [expr rand()*$val(stop)]
 	set mov_speed_ [expr rand()*$val(speed)]
 	$ns_ at $rng_time "$node_($i) setdest $x_ $y_ $mov_speed_"
-	# $ns_ at $rng_time "$node_($i) setdest $xx_ $yy_ 15.0"
 }
-
-# Node_(1) starts to move towards node_(0)
-# $ns_ at 50.0 "$node_(1) setdest 25.0 20.0 15.0"
-# $ns_ at 10.0 "$node_(0) setdest 20.0 18.0 1.0"
-
-# Node_(1) then starts to move away from node_(0)
-# $ns_ at 100.0 "$node_(1) setdest 490.0 480.0 15.0" 
-# $ns_ at 50.0 "$node_(2) setdest 369 170 3"
 
 # Define node initial position in nam
 for {set i 0} {$i < $val(nn)} {incr i} {
@@ -93,12 +88,33 @@ $ns_ attach-agent $node_(1) $sink
 $ns_ connect $tcp $sink
 set ftp [new Application/FTP]
 $ftp attach-agent $tcp
-$ns_ at 10.0 "$ftp start" 
+$ns_ at 5.0 "$ftp start" 
+
+set tcp1 [new Agent/TCP]
+$tcp1 set class_ 2
+set sink1 [new Agent/TCPSink]
+$ns_ attach-agent $node_(0) $tcp1
+$ns_ attach-agent $node_(11) $sink1
+$ns_ connect $tcp1 $sink1
+set ftp1 [new Application/FTP]
+$ftp1 attach-agent $tcp1
+$ns_ at 7.0 "$ftp1 start"
+
+set tcp2 [new Agent/TCP]
+$tcp2 set class_ 2
+set sink2 [new Agent/TCPSink]
+$ns_ attach-agent $node_(10) $tcp2
+$ns_ attach-agent $node_(11) $sink2
+$ns_ connect $tcp2 $sink2
+set ftp2 [new Application/FTP]
+$ftp2 attach-agent $tcp2
+$ns_ at 10.0 "$ftp2 start"
 
 # Tell nodes when the simulation ends
 for {set i 0} {$i < $val(nn) } {incr i} {
     $ns_ at 150.0 "$node_($i) reset";
 }
+
 $ns_ at 150.0001 "stop"
 $ns_ at 150.0002 "puts \"NS EXITING...\" ; $ns_ halt"
 proc stop {} {
@@ -110,7 +126,7 @@ proc stop {} {
 }
 
 proc finish {} {
-exec xgraph simple.tr -geometry 1000*1000
+exec xgraph sample.tr -geometry 1000*1000
 exit 0
 }
 
